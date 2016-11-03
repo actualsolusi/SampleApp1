204a92c746ff8bf88055eecd00f77a5621439a7f
erick <erick@DESKTOP-I963S2R>
2016-11-03 16:50:39 +0700

    menambahkan CategoryDAL

---
+++ DAL/CategoryDAL.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+Imports BO
+Imports DAL
+Imports Dapper
+
+Public Class CategoryDAL
+    Implements ICrud(Of Category)
+
+    Private Function GetConn() As String
+        Return ConfigurationManager.ConnectionStrings("SampleShopDbConnectionString").ConnectionString
+    End Function
+
+
+    Public Sub Create(model As Category) Implements ICrud(Of Category).Create
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub Delete(id As String) Implements ICrud(Of Category).Delete
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub Update(model As Category) Implements ICrud(Of Category).Update
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Function GetAll() As IEnumerable(Of Category) Implements ICrud(Of Category).GetAll
+        Using conn As New SqlConnection(GetConn())
+            Dim strSql = "select * from Categories 
+                          order by CategoryName"
+
+            Dim results = conn.Query(Of Category)(strSql)
+
+            Return results
+        End Using
+    End Function
+
+    Public Function GetByID(id As String) As Category Implements ICrud(Of Category).GetByID
+        Using conn As New SqlConnection(GetConn())
+            Dim strSql = "select * from Categories 
+                          where CategoryID=@CategoryID"
+
+            Dim params = New With {.CategoryID = id}
+
+            Dim results = conn.Query(Of Category)(strSql, params)
+
+            Return results.SingleOrDefault()
+        End Using
+    End Function
+End Class

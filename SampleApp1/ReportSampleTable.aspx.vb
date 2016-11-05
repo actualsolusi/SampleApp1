7814121c663bdea1cee8ffadc89e9d2df0cd360a
erick <erick@DESKTOP-I963S2R>
2016-11-05 17:03:27 +0700

    sample print pdf

---
+++ SampleApp1/ReportSampleTable.aspx.vb
@@ -0,0 +1,17 @@
+﻿Imports BO
+Imports DAL
+Imports Microsoft.Reporting.WebForms
+Public Class ReportSampleTable
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not Page.IsPostBack Then
+
+            Dim reportSampleDAL As New ReportSampleTableDAL
+            Dim results = reportSampleDAL.GetAll()
+            Dim ds As New ReportDataSource("DataSet1", results)
+            ReportViewer1.LocalReport.DataSources.Add(ds)
+        End If
+    End Sub
+
+End Class

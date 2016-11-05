0b73dd3bae3b5757740061e2406a5b2e9c36b807
erick <erick@DESKTOP-I963S2R>
2016-11-05 15:38:15 +0700

    menggunakan json.net

---
+++ SampleApp1/SampleBacaSON.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="SampleBacaSON.aspx.vb" Inherits="SampleApp1.SampleBacaSON" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>JSON Sample</h2>
+    <br />
+    Masukan JSON :<br />
+    <asp:TextBox runat="server" Width="600" ID="txtJSONString" TextMode="MultiLine" Rows="20" />
+    <br /><br />
+    <asp:Button Text="Baca JSON" ID="btnBacaJson" runat="server" /><br />
+
+    <asp:Label ID="lblIsiJSON" runat="server" />
+
+
+</asp:Content>
---
+++ SampleApp1/SampleBacaSON.aspx.vb
@@ -0,0 +1,18 @@
+﻿Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Public Class SampleBacaSON
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnBacaJson_Click(sender As Object, e As EventArgs) Handles btnBacaJson.Click
+        Dim data As JObject = JObject.Parse(txtJSONString.Text)
+
+        Dim kdJenisPeserta As String = CType(data("response")("peserta")("jenisPeserta")("kdJenisPeserta"), String)
+        Dim nmJenisPeserta As String = CType(data("response")("peserta")("jenisPeserta")("nmJenisPeserta"), String)
+        lblIsiJSON.Text = "Kode :" & kdJenisPeserta & " - Jenis Peserta : " & nmJenisPeserta
+    End Sub
+End Class
